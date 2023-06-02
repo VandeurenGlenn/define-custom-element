4ef7ef55426fa1093a421cafaa7cf10284c1f222
Glenn Vandeuren <vandeurenglenn@gmail.com>
2023-06-02 19:29:30 +0200

    Remove dependencies & port to module

--- index.js
+++ index.js
@@ -1,25 +1,13 @@
-'use strict';
-
-var hyphenate = string => {
-  const firstLetter = string.charAt(0);
-  string = string.replace(firstLetter, firstLetter.toLowerCase());
-  const uppercases = string.match(/([A-Z])/g);
-
- for (const uppercase of uppercases) {
-   string = string.replace(uppercase, `-${uppercase.toLowerCase()}`);
- }
- return string
-};
+const hyphenate = input => input.replace(/([a-zA-Z])(?=[A-Z])/g, '$1-').toLowerCase();
 
 const shouldDefine = name => customElements.get(name) ? false : true;
 
-var index = klass => {
+export default klass => {
   if (!klass) return console.error('class undefined');
   if (!klass.constructor) return console.error('constructor required');
   if (!klass.name) return console.error('class name required');
 
   const name = hyphenate(klass.name);
   return shouldDefine(name) ? customElements.define(name, klass) : '';
-};
-
-module.exports = index;
+}
+  
